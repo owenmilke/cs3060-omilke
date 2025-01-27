1f3e806d18a5199e613cdc3dedb0b8c04ca72e78
owenmilke <owenmilke66@gmail.com>
2025-01-27 17:36:24 -0500

    Finished onelink

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time
 physicsClient = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+p.loadSDF("box.sdf")
+
 for x in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
