70145353be39833c6810d6f1bec7bf67a676a74b
owenmilke <owenmilke66@gmail.com>
2025-02-05 14:57:57 -0500

    Changed time limit

--- simulate.py
+++ simulate.py
@@ -11,7 +11,7 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
-for i in range(1000):
+for i in range(10000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
