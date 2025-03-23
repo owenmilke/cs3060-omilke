2c8b4096d97008b75fd313d39e7e7008ee534395
owenmilke <owenmilke66@gmail.com>
2025-03-22 20:03:36 -0400

    Finished quadruped

--- constants.py
+++ constants.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 num_iterations = 1000
+
 num_generations = 10
 populationSize = 10
+numSensorNeurons = 4
+numMotorNeurons = 8
+motorJointRange = 0.2
+
 time_step = [1/30, 1/60, 1/240]
 
 maxForce = 100
--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ from motor import MOTOR
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 from pyrosim.neuralNetwork import NEURAL_NETWORK
+import constants as c
 import os
 
 class ROBOT:
@@ -33,7 +34,7 @@ class ROBOT:
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
-                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
                 self.motors[jointName].Set_Value(self.robotId, desiredAngle)
 
     def Think(self):
